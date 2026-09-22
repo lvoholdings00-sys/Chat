8a82dfee80fcb318444cb14326ac1265621fb490
lvoholdings00-sys <lvoholdings00@gmail.com>
2026-09-21 22:39:01 -0500

    Update server.ts

--- server.ts
+++ server.ts
@@ -8,7 +8,7 @@ import type { User, Channel, Message, CalendarEvent, NewsPost, ScheduledMessage,
 
 dotenv.config();
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 const app = express();
 const server = http.createServer(app);
 
